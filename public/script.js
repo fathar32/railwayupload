8be7dc0bd13fd1497cb3f8fabf8dc272960e6199
fathar32 <117836565+fathar32@users.noreply.github.com>
2025-11-17 11:48:13 +0800

    Create script.js

---
+++ public/script.js
@@ -0,0 +1,29 @@
+
+const form = document.getElementById('uploadForm');
+const resultBox = document.getElementById('result');
+
+form.addEventListener('submit', async (e) => {
+  e.preventDefault();
+
+  const formData = new FormData(form);
+
+  try {
+    const response = await fetch('/upload-csv', {
+      method: 'POST',
+      body: formData
+    });
+
+    if (response.ok) {
+      const text = await response.text();
+      resultBox.innerText = "✅ " + text;
+      resultBox.style.color = "green";
+    } else {
+      const errorText = await response.text();
+      resultBox.innerText = "❌ Upload gagal: " + errorText;
+      resultBox.style.color = "red";
+    }
+  } catch (err) {
+    resultBox.innerText = "❌ Terjadi error saat upload!";
+    resultBox.style.color = "red";
+  }
+});
